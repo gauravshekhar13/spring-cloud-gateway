b8c3f1d0f001a4982eb5ff2dce39fb85ee0c72c0
Gaurav <gaurav@gmail.com>
2020-05-19 10:26:31 -0500

    lombok addition

--- src/main/java/com/example/demo/GatewaySampleApplication.java
+++ src/main/java/com/example/demo/GatewaySampleApplication.java
@@ -9,11 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
 @SpringBootApplication
-
+@Slf4j
 public class GatewaySampleApplication {
+	
+	private static final org.slf4j.Logger log = 
+		    org.slf4j.LoggerFactory.getLogger(GatewaySampleApplication.class);
 
 	@Value("http://localhost:8081")
 	private URI homeAsiaPacific;
@@ -28,8 +33,8 @@ public class GatewaySampleApplication {
 		ResponseEntity<?> rs = null;
 		if(genericTSP.getTokenId().equals("11")) {
 			
-			System.out.println("in proxy method");
-			System.out.println("URL is:" + homeAsiaPacific.toString());
+			log.info("in proxy method");
+			log.info("URL is:" + homeAsiaPacific.toString());
 			rs = proxy.uri(homeAsiaPacific.toString() + "/asiapacific/tspIndia").get();
 		}
 		
@@ -43,16 +48,16 @@ public class GatewaySampleApplication {
 	@GetMapping("/callIndia")
 	public ResponseEntity<?> proxyIndia(ProxyExchange<Object> proxy) throws Exception {
 
-		System.out.println("in proxy method");
-		System.out.println("URL is:" + homeAsiaPacific.toString());
+		log.info("in proxy method");
+		log.info("URL is:" + homeAsiaPacific.toString());
 		return proxy.uri(homeAsiaPacific.toString() + "/asiapacific/tspIndia").get();
 	}
 	
 	@GetMapping("/callChina")
 	public ResponseEntity<?> proxyJapan(ProxyExchange<Object> proxy) throws Exception {
 
-		System.out.println("in proxy method");
-		System.out.println("URL is:" + homeAsiaPacific.toString());
+		log.info("in proxy method");
+		log.info("URL is:" + homeAsiaPacific.toString());
 		return proxy.uri(homeAsiaPacific.toString() + "/asiapacific/tspChina").get();
 	}
 	
