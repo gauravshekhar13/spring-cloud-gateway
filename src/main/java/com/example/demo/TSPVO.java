d94f906933482fe8233eb64b8c82c8a7ecac26bd
Gaurav <gaurav@gmail.com>
2020-05-18 21:58:11 -0500

    new chnages

---
+++ src/main/java/com/example/demo/TSPVO.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class TSPVO {
+private String name;
+private String location;
+private String message;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+	
+}
--- src/main/java/com/example/demo/TspIndiaController.java
+++ src/main/java/com/example/demo/TspIndiaController.java
@@ -1,18 +1,37 @@
 package com.example.demo;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/asiapacific")
 public class TspIndiaController {
 	@GetMapping("/tspIndia")
-	public String tspIndia() {
-		return "This is TSP India";
+	public ResponseEntity<?> tspIndia() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("custom-header", "TSPINDIA");
+		
+		TSPVO tspvo1 = new TSPVO();
+		tspvo1.setMessage("This is TSP India");
+		tspvo1.setLocation("India");
+		tspvo1.setName("TSP");
+		
+		return new ResponseEntity<TSPVO>(tspvo1,httpHeaders,HttpStatus.OK);
 	}
 	
 	@GetMapping("/tspChina")
-	public String tspChina() {
-		return "This is TSP China";
+	public ResponseEntity<?> tspChina() {
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("custom-header", "TSPCHINA");
+		TSPVO tspvo1 = new TSPVO();
+		tspvo1.setMessage("This is TSP CHina");
+		tspvo1.setLocation("China");
+		tspvo1.setName("TSP");
+		
+		return new ResponseEntity<TSPVO>(tspvo1,httpHeaders,HttpStatus.OK);
 	}
 	
 	@GetMapping("/tspJapan")
